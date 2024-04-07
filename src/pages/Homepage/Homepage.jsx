8bd3c4b21df78d634763aa60f10ad2ded5bbec67
leejeongseung1 <jki1256@hanmail.net>
2024-04-07 22:52:16 +0900

    6~7강 내용 반영

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,16 @@
-export const Homepage = () => {
+/*
+    TODO:
+     1. 배너 만들기 => popular 영화를 들고와서 첫번째 아이템을 보여주자.
+     2. popular movie
+     3. top rated movie
+     4. upcoming movie
+*/
+import {Banner} from "./components/Banner/Banner";
+
+ export const Homepage = () => {
     return (
-        <>Homepage</>
+        <div>
+            <Banner></Banner>
+        </div>
     )
 }
---
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -0,0 +1,43 @@
+import {usePopularMoviesQuery} from "../../../../hooks/UsePopularMovies";
+import {Alert, Button, Spinner} from "react-bootstrap";
+import './Banner.style.css'
+
+export const Banner = () => {
+
+    const {data, isLoading, isError, error} = usePopularMoviesQuery();
+    console.log('ddd', data);
+    if (isLoading) {
+        return (
+            <div>
+                <Spinner animation="border" variant="danger"/>
+            </div>
+        )
+    }
+    if (isError) {
+        return (
+            <Alert variant="danger">
+                <Alert.Heading>Error</Alert.Heading>
+                <p>{error}</p>
+                <hr/>
+                <div className="d-flex justify-content-end">
+                    <Button variant="outline-success">
+                        메인 페이지로
+                    </Button>
+                </div>
+            </Alert>
+        )
+    }
+
+    return (
+        <div style={{
+            backgroundImage: `url("https://media.themoviedb.org/t/p/original${data?.results[0].poster_path}")`
+        }}
+             className="Banner"
+        >
+            <div className="text-white banner-text-area">
+                <h1>{data?.results[0].title}</h1>
+                <p>{data?.results[0].overview}</p>
+            </div>
+        </div>
+    )
+}
